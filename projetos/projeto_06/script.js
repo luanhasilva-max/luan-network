95e17aa74f94519a2a5b4789be339da6acb7413a
LUAN HENRIQUE ARAÚJO SILVA <luan_ha_silva@estudante.sesisenai.org.br>
2026-08-19 16:17:49 -0300

    atividade

--- projetos/projeto_06/script.js
+++ projetos/projeto_06/script.js
@@ -11,7 +11,7 @@ btnEnviar.addEventListener("click", function () {
 
 inputText.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    adicionarTarefa()
+    adicionarTarefa();
   }
 });
 
@@ -22,6 +22,30 @@ function adicionarTarefa() {
     contadorId++;
     tarefas.push({ id: contadorId, tarefa: texto, concluido: false });
     inputText.value = "";
-    console.log(tarefas);
+    renderizar();
   }
+}
+
+
+function renderizar() {
+  lista.innerHTML = ""
+  tarefas.forEach(function (item) {
+    let li = document.createElement("li");
+    li.textContent = item.tarefa;
+  
+    lista.appendChild(li);
+
+    let btnExcluir = document.createElement("button")
+    btnExcluir.innerText = "❌"
+    li.appendChild(btnExcluir)
+    btnExcluir.addEventListener("click", function(e){
+      e.stopPropagation()
+
+      tarefas = tarefas.filter(function(t){
+        return t.id !== item.id
+
+      })
+      renderizar()
+    })
+  });
 }
